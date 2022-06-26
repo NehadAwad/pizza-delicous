02a8d29843b91041776c536b9cf4da201a22b16c
NehadAwad <nehadakash622@gmail.com>
2022-06-26 23:20:16 +0600

    ejs frontend added

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mysql = require('mysql')
+
+mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pizza_node"
+})
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(8080);
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res)=>{
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "pizza_node"
+    })
+    con.query("SELECT * FROM products", (err, result) =>{
+      res.render('pages/index', {result:result});
+    })    
+    
+}); 
+
+const start = async () => {
+    try {
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+};
+  
+start();
